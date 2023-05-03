81242653b89eca1d25f8ed0b7ecb79f9972045ce
snorkysnark <snorkysnark@gmail.com>
2023-05-03 16:55:26 +0200

    connecting to sqlalchemy and executing sql script

--- csvload.py
+++ csvload.py
@@ -1,21 +1,27 @@
+from argparse import ArgumentParser
 from pathlib import Path
-from typing import Optional
+
 import sqlglot
-from sqlglot.expressions import Expression
+import sqlparse
+import sqlalchemy
 
+from csvload.parser import AnnotatedTable
 
-def is_create_table(expression: Optional[Expression]):
-    return (
-        expression
-        and expression.key == "create"
-        and expression.args["kind"].lower() == "table"
-    )
+if __name__ == "__main__":
+    argparser = ArgumentParser()
+    argparser.add_argument("db", help="sqlalchemy database url")
+    argparser.add_argument("sql", type=Path, help="annotated sql file")
+    # argparser.add_argument("data", type=Path, help="csv data file")
+    args = argparser.parse_args()
 
+    sql_script = args.sql.read_text()
+    parsed_statements = sqlglot.parse(sql_script)
+    table_info = AnnotatedTable.from_statements(parsed_statements, {"group": "egg_irl"})
 
-expressions = sqlglot.parse(Path("./example.sql").read_text())
-create_table = next(expr for expr in expressions if is_create_table(expr))
-assert create_table
-
-columns = create_table.args["this"].args["expressions"]
-
-print(create_table)
+    engine = sqlalchemy.create_engine(args.db, echo=True)
+    with engine.connect() as conn:
+        # Have to use sqlparse here, not sqlglot,
+        # since converting parsed_statements back to string leaves some
+        # db-specific keywords (like AUTOINCREMENT) corrupted
+        for statement in sqlparse.split(sql_script):
+            conn.execute(sqlalchemy.text(statement))
